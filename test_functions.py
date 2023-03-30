1ee22095f4251820de47a7686daddda8ed34131d
Pieter van Doorn <pvandoorn@gmail.com>
2023-03-30 11:00:08 +0200

    add multiply test

--- test_functions.py
+++ test_functions.py
@@ -1,5 +1,5 @@
 from functions import add, subtract, multiply
-from functions import convert_fahrenheit_to_celsius as f2c
+# from functions import convert_fahrenheit_to_celsius as f2c
 import pytest
 
 
@@ -7,6 +7,8 @@ def test_add():
     assert add(2, 3) == 5
     assert add("space", "ship") == "spaceship"
 
+def test_multiply():
+    assert multiply(3, 4) == 12
 
 # def test_subtract():
 #    assert subtract(3,2) == 1
